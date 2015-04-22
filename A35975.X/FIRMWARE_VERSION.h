a22a687fcd3a78aea4a709302b915e2fa59bf158
dqin66github <dqin@etm-inc.com>
2015-04-22 13:58:11 -0700

    91-35995-250 rev 1
    build for production

--- A35975.X/FIRMWARE_VERSION.h
+++ A35975.X/FIRMWARE_VERSION.h
@@ -1,9 +1,9 @@
 #ifndef __FIRMWARE_VERISON_H
 #define __FIRMWARE_VERISON_H
 
-#define FIRMWARE_AGILE_REV     0
+#define FIRMWARE_AGILE_REV     1
 #define FIRMWARE_BRANCH        0
-#define FIRMWARE_MINOR_REV     1
+#define FIRMWARE_MINOR_REV     0
 
 
 #endif
--- A35975.X/Version.h
+++
@@ -1,6 +0,0 @@
-#ifndef __VERSION_H
-#define __VERSION_H
-
-#define VERSION_NUMBER 0x003A
-
-#endif
